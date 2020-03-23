cb61c5bb8bc8220e997658a0b3cc23d58e2634e8
Maz101 <43634935+Maz101@users.noreply.github.com>
2020-03-23 16:51:25 +0000

    Add files via upload

--- elasticshell.sh
+++ elasticshell.sh
@@ -9,10 +9,7 @@ cd elasticsearch-7.6.1/bin
 ./elasticsearch -Epath.data=data2 -Epath.logs=log2
 ./elasticsearch -Epath.data=data3 -Epath.logs=log3
 
-git clone https://github.com/CHjasonxu/Iformation-Retrieval.git
 
-curl -H "Content-Type: application/json" -XPOST "localhost:9200/bank/_bulk?pretty&refresh" --data-binary "@Netflix.json"
-curl "localhost:9200/_cat/indices?v"
 
 
 
