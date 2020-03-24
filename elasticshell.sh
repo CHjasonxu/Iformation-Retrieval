0e1adbf2e5241c99fa5e5f07f89fcb441849b098
Maz101 <43634935+Maz101@users.noreply.github.com>
2020-03-24 10:45:12 +0000

    Update elasticshell.sh

--- elasticshell.sh
+++ elasticshell.sh
@@ -1,15 +1,18 @@
 ##Linux
 
-curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.6.1-linux-x86_64.tar.gz
-tar -xvf elasticsearch-7.6.1-linux-x86_64.tar.gz
+#curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.6.1-linux-x86_64.tar.gz
+#tar -xvf elasticsearch-7.6.1-linux-x86_64.tar.gz
+
+## macOS
+#curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.6.1-darwin-x86_64.tar.gz
+#tar -xvf elasticsearch-7.6.1-darwin-x86_64.tar.gz
+
 
 cd elasticsearch-7.6.1/bin
 ./elasticsearch
 
 ./elasticsearch -Epath.data=data2 -Epath.logs=log2
 ./elasticsearch -Epath.data=data3 -Epath.logs=log3
-
-
 
 
 
