eebb141887bcf4eb545d8d7112ffd0731a1cb0d1
Maz101 <43634935+Maz101@users.noreply.github.com>
2020-03-24 21:31:15 +0000

    Add files via upload

---
+++ SearchEngineIn.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+from datetime import datetime
+from elasticsearch import Elasticsearch
+import csv, json
+import pandas as pd
+es = Elasticsearch()
+
+
+# In[9]:
+
+
+data = {}
+idd = {}
+line2 = {}
+listline2 = []
+listdata = []
+director = {}
+
+with open('/home/maz/Information Retrieval/Search Engine/NetflixData.csv') as csvfile:
+    csvReader = csv.DictReader(csvfile)
+    i=1
+    for rows in csvReader:
+        idd["_id"] = rows['show_id']
+        data["index"] = idd
+
+        line2["type"] = rows['type']
+        line2["title"] = str(rows['title']).replace("'","")
+        
+        if not rows["director"]:
+            line2["director"] = ['None']
+        else:
+            line2["director"] = rows['director'].split(',')
+            
+        if not rows['cast']:
+            line2["cast"] = ['None']
+        else:        
+            line2["cast"] = rows['cast'].split(',')
+            
+            
+        if not rows['country']:
+            line2["country"] = ['None']
+        else:
+            line2["country"] = rows['country'].split(',')
+        line2["date_added"] = rows['date_added']
+        line2["release_year"] = rows['release_year']
+        line2["rating"] = rows['rating']
+        line2["duration"] = rows['duration']
+        
+        if not rows['listed_in']:
+            line2["listed_in"] = ['None']
+        else:
+            line2["description"] = rows['description']
+            
+        line2["timestamp"] = datetime.now()
+            
+        doc = line2
+        
+
+        res = es.index(index="netflixdata", id=i, body=doc)
+        i+=1
+        print(res['result'])
+
+
+            
+        
+
+
+# In[11]:
+
+
+es.indices.refresh(index="netflixdata")
+
+res = es.search(index="netflixdata", body={"query": {"match_all": {}}})
+print("Got %d Hits:" % res['hits']['total']['value'])
+for hit in res['hits']['hits']:
+    print("%(timestamp)s %(title)s: %(description)s" % hit["_source"])
+
