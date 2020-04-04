b9589e30ee692de540efeb3eb04ae6283d8f42fc
Maz101 <43634935+Maz101@users.noreply.github.com>
2020-04-04 23:06:00 +0100

    Add files via upload

--- SearchEngineIn.py
+++ SearchEngineIn.py
@@ -18,6 +18,17 @@ listline2 = []
 listdata = []
 director = {}
 
+
+##basicmodel be,d,g,if,in,ine   after_effect no,b,l          normalization = no,h1,h2,h3,z  
+## normalization.h2/h1/h3/z/c (float values)
+
+jsondfrtext = '{"settings":{"index":{"similarity":{"my_similarity":{"type": "DFR","basic_model": "g","after_effect": "l","normalization": "h3","normalization.h2.c": "3.0"}}}}}'
+
+headers = {'Content-type': 'application/json',}
+
+DFRtext = requests.put('http://localhost:9200/netflixdata_dfr?pretty' , headers=headers, data=jsondfrtext)
+
+
 with open('netflix_titles.csv', 'r', encoding='utf-8') as csvfile:
     csvReader = csv.DictReader(csvfile)
     i=1
@@ -58,30 +69,99 @@ with open('netflix_titles.csv', 'r', encoding='utf-8') as csvfile:
         doc = line2
         
 
-        res = es.index(index="netflixdata", id=i, body=doc)
+        res = es.index(index="netflixdata_dfr", id=i, body=doc)
         i+=1
      
 
 
 
-es.indices.refresh(index="netflixdata")
+es.indices.refresh(index="netflixdata_dfr")
 
-res = es.search(index="netflixdata", body={"query": {"match_all": {}}})
+res = es.search(index="netflixdata_dfr", body={"query": {"match_all": {}}})
 print("Got %d Hits:" % res['hits']['total']['value'])
 for hit in res['hits']['hits']:
     print("%(timestamp)s %(title)s: %(description)s" % hit["_source"])
 
 
-##basicmodel be,d,g,if,in,ine   after_effect no,b,l          normalization = no,h1,h2,h3,z  
-## normalization.h2/h1/h3/z/c (float values)
 
-jsontext = '{"settings":{"index":{"similarity":{"my_similarity":{"type": "DFR","basic_model": "g","after_effect": "l","normalization": "h3","normalization.h2.c": "3.0"}}}}}'
+
+print(DFRtext.json())
+DFRsettings = requests.get('http://localhost:9200/netflixdata_dfr/_settings?pretty' , headers=headers)
+print(DFRsettings.json())
+
+
+
+
+##bm25 
+##bm25 ##CAN PUT BM25 ON SPECIFIC FIELDS IN THE MAPPING! and the rest use defaul similarity
+## {  "settings": {    "similarity": {      "my_custom_similarity": {        "type": "BM25",        "b": 0,        "k1": 2         }    }  },  "mappings": {    "doc": {      "properties": {        "cast": {          "type":       "text"        },        "country": {          "type":       "text"        },{        "date_added": {          "type":       "text"        },        "description": {          "type":       "text",          "similarity": "jsonbm25text"                },        {        "director": {          "type":       "text"        },        "duration": {          "type":       "text"        },{        "rating": {          "type":       "text"        },        "release_year": {          "type":       "text"        },        {        "timestamp": {          "type":       "text"        },        "title": {          "type":       "text"        },{        "rating": {          "type":       "text"        }   }    }  }}'
+
+jsonbm25text = '{"settings":{"index":{"similarity":{"my_bm25__similarity":{"type":"BM25","b": "0","k1": "2"}}}}}'
 
 headers = {'Content-type': 'application/json',}
 
-DFRtext = requests.put('http://localhost:9200/index?pretty' , headers=headers, data=jsontext)
+bm25text = requests.put('http://localhost:9200/netflixdata_bm25?pretty' , headers=headers, data=jsondfrtext)
 
-print(DFRtext.json())
+
+with open('netflix_titles.csv', 'r', encoding='utf-8') as csvfile:
+    csvReader = csv.DictReader(csvfile)
+    i=1
+    for rows in csvReader:
+        idd["_id"] = rows['show_id']
+        data["index"] = idd
+
+        line2["type"] = rows['type']
+        line2["title"] = rows['title']
+        
+        if not rows["director"]:
+            line2["director"] = ['None']
+        else:
+            line2["director"] = rows['director'].split(',')
+            
+        if not rows['cast']:
+            line2["cast"] = ['None']
+        else:        
+            line2["cast"] = rows['cast'].split(',')
+            
+            
+        if not rows['country']:
+            line2["country"] = ['None']
+        else:
+            line2["country"] = rows['country'].split(',')
+        line2["date_added"] = rows['date_added']
+        line2["release_year"] = rows['release_year']
+        line2["rating"] = rows['rating']
+        line2["duration"] = rows['duration']
+        
+        if not rows['listed_in']:
+            line2["listed_in"] = ['None']
+        else:
+            line2["description"] = rows['description']
+            
+        line2["timestamp"] = datetime.now()
+            
+        doc = line2
+        
+
+        res = es.index(index="netflixdata_bm25", id=i, body=doc)
+        i+=1
+     
+
+
+
+es.indices.refresh(index="netflixdata_bm25")
+
+res = es.search(index="netflixdata_bm25", body={"query": {"match_all": {}}})
+print("Got %d Hits:" % res['hits']['total']['value'])
+for hit in res['hits']['hits']:
+    print("%(timestamp)s %(title)s: %(description)s" % hit["_source"])
+
+
+
+
+print(bm25text.json())
+bm25settings = requests.get('http://localhost:9200/netflixdata_bm25/_settings?pretty' , headers=headers)
+print(bm25settings.json())
 
 
 
