e19b02d5429dd4cac168665dbf756ab485b357e6
Maz101 <43634935+Maz101@users.noreply.github.com>
2020-04-10 12:31:15 +0100

    Add files via upload

--- SearchEngineIn.py
+++ SearchEngineIn.py
@@ -26,13 +26,26 @@ headers = {'Content-type': 'application/json',}
 DFRtext = requests.put('http://localhost:9200/dfrnetflix?pretty' , headers=headers, data=jsondfrtext)
 
 
-##bm25 
-##bm25 ##CAN PUT BM25 ON SPECIFIC FIELDS IN THE MAPPING! and the rest use defaul similarity
-## {  "settings": {    "similarity": {      "my_custom_similarity": {        "type": "BM25",        "b": 0,        "k1": 2         }    }  },  "mappings": {    "doc": {      "properties": {        "cast": {          "type":       "text"        },        "country": {          "type":       "text"        },{        "date_added": {          "type":       "text"        },        "description": {          "type":       "text",          "similarity": "my_custom_similarity"                },        {        "director": {          "type":       "text"        },        "duration": {          "type":       "text"        },{        "rating": {          "type":       "text"        },        "release_year": {          "type":       "text"        },        {        "timestamp": {          "type":       "text"        },        "title": {          "type":       "text"        },{        "rating": {          "type":       "text"        }   }    }  }}'
+myismapping = '{"properties": {"cast": {"type": "text","similarity": "my_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"country": {"type": "text","similarity": "my_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"date_added": {"type": "text","similarity": "my_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"description": {"type": "text","similarity": "my_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 1000}}},"director": {"type": "text","similarity": "my_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"duration": {"type": "text","similarity": "my_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"rating": {"type": "text", "similarity": "my_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"release_year": {"type": "text","similarity": "my_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"title": {"type": "text","similarity": "my_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"type": {"type": "text","similarity":"my_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}}}}'
 
+myismaptext = requests.put('http://localhost:9200/dfrnetflix/_mapping?pretty' , headers=headers, data = myismapping)
+
+
+print(DFRtext.json())
+print(myismaptext.json())
+
+
+
+
+##bm25 
 jsonbm25text = '{"settings":{"index":{"similarity":{"my_bm25__similarity":{"type":"BM25","b": "0","k1": "2"}}}}}'
 headers = {'Content-type': 'application/json',}
 bm25text = requests.put('http://localhost:9200/bm25netflix?pretty' , headers=headers, data=jsonbm25text)
+
+
+print(bm25text.json())
+
+
 
 
 ### Scripted Similarity
@@ -40,11 +53,30 @@ jsontfidftext = '{"settings":{"index":{"similarity":{"scripted_tfidf":{"type": "
 headers = {'Content-type': 'application/json',}
 tfidftext = requests.put('http://localhost:9200/tfidfnetflix?pretty' , headers=headers, data=jsontfidftext)
 
+scrmapping = '{"properties": {"cast": {"type": "text","similarity": "scripted_tfidf","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"country": {"type": "text","similarity": "scripted_tfidf","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"date_added": {"type": "text","similarity": "scripted_tfidf","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"description": {"type": "text","similarity": "scripted_tfidf","fields": {"keyword": {"type": "keyword","ignore_above": 1000}}},"director": {"type": "text","similarity": "scripted_tfidf","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"duration": {"type": "text","similarity": "scripted_tfidf","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"rating": {"type": "text", "similarity": "scripted_tfidf","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"release_year": {"type": "text","similarity": "scripted_tfidf","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"title": {"type": "text","similarity": "scripted_tfidf","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"type": {"type": "text","similarity":"scripted_tfidf","fields": {"keyword": {"type": "keyword","ignore_above": 256}}}}}'
+
+tfidfmaptext = requests.put('http://localhost:9200/tfidfnetflix/_mapping?pretty' , headers=headers, data = scrmapping)
+
+print(tfidftext.json())
+print(tfidfmaptext.json())
+
+
+
+
 
 ###DFI similarity
 jsondfitext = '{"settings":{"index":{"similarity":{"dfi":{"type": "DFI","independence_measure":"standardized"}}}}}'
 headers = {'Content-type': 'application/json',}
 dfitext = requests.put('http://localhost:9200/dfinetflix?pretty' , headers=headers, data=jsondfitext)
+
+dfimapping = '{"properties": {"cast": {"type": "text","similarity": "dfi","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"country": {"type": "text","similarity": "dfi","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"date_added": {"type": "text","similarity": "dfi","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"description": {"type": "text","similarity": "dfi","fields": {"keyword": {"type": "keyword","ignore_above": 1000}}},"director": {"type": "text","similarity": "dfi","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"duration": {"type": "text","similarity": "dfi","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"rating": {"type": "text", "similarity": "dfi","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"release_year": {"type": "text","similarity": "dfi","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"title": {"type": "text","similarity": "dfi","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"type": {"type": "text","similarity":"dfi","fields": {"keyword": {"type": "keyword","ignore_above": 256}}}}}'
+
+dfimaptext = requests.put('http://localhost:9200/dfinetflix/_mapping?pretty' , headers=headers, data = dfimapping)
+
+print(dfitext.json())
+print(dfimaptext.json())
+
+
 
 
 ##LM Dircichlet similarity
@@ -52,17 +84,43 @@ jsonlmdtext = '{"settings":{"index":{"similarity":{"esserverbook_lm_dircichlet_s
 headers = {'Content-type': 'application/json',}
 lmdtext = requests.put('http://localhost:9200/lmdnetflix?pretty' , headers=headers, data=jsonlmdtext)
 
+lmdmapping = '{"properties": {"cast": {"type": "text","similarity": "esserverbook_lm_dircichlet_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"country": {"type": "text","similarity": "esserverbook_lm_dircichlet_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"date_added": {"type": "text","similarity": "esserverbook_lm_dircichlet_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"description": {"type": "text","similarity": "esserverbook_lm_dircichlet_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 1000}}},"director": {"type": "text","similarity": "esserverbook_lm_dircichlet_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"duration": {"type": "text","similarity": "esserverbook_lm_dircichlet_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"rating": {"type": "text", "similarity": "esserverbook_lm_dircichlet_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"release_year": {"type": "text","similarity": "esserverbook_lm_dircichlet_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"title": {"type": "text","similarity": "esserverbook_lm_dircichlet_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"type": {"type": "text","similarity":"esserverbook_lm_dircichlet_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}}}}'
+
+lmdmaptext = requests.put('http://localhost:9200/lmdnetflix/_mapping?pretty' , headers=headers, data = lmdmapping)
+
+print(lmdtext.json())
+print(lmdmaptext.json())
+
+
+
 
 ##LM Jelinek Mercer Similarity
 jsonlmjtext = '{"settings":{"index":{"similarity":{"esserverbook_lm_jelinek_mercer_similarity":{"type": "LMJelinekMercer","lambda":"0.7"}}}}}'
 headers = {'Content-type': 'application/json',}
 lmjtext = requests.put('http://localhost:9200/lmjnetflix?pretty' , headers=headers, data=jsonlmjtext)
 
+lmjmapping = '{"properties": {"cast": {"type": "text","similarity": "esserverbook_lm_jelinek_mercer_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"country": {"type": "text","similarity": "esserverbook_lm_jelinek_mercer_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"date_added": {"type": "text","similarity": "esserverbook_lm_jelinek_mercer_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"description": {"type": "text","similarity": "esserverbook_lm_jelinek_mercer_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 1000}}},"director": {"type": "text","similarity": "esserverbook_lm_jelinek_mercer_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"duration": {"type": "text","similarity": "esserverbook_lm_jelinek_mercer_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"rating": {"type": "text", "similarity": "esserverbook_lm_jelinek_mercer_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"release_year": {"type": "text","similarity": "esserverbook_lm_jelinek_mercer_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"title": {"type": "text","similarity": "esserverbook_lm_jelinek_mercer_similarity", "fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"type": {"type": "text","similarity":"esserverbook_lm_jelinek_mercer_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}}}}'
+
+lmjmaptext = requests.put('http://localhost:9200/lmjnetflix/_mapping?pretty' , headers=headers, data = lmjmapping)
+
+print(lmjtext.json())
+print(lmjmaptext.json())
+
+
+
+
 
 ##IB Similarity
 jsonibtext = '{"settings":{"index":{"similarity":{"esserverbook_ib_similarity":{"type": "IB","distribution":"ll","lambda":"df","normalization":"z","normalization.z.z":"0.25"}}}}}'
 headers = {'Content-type': 'application/json',}
 ibtext = requests.put('http://localhost:9200/ibnetflix?pretty' , headers=headers, data=jsonibtext)
+
+ibmapping = '{"properties": {"cast": {"type": "text","similarity": "esserverbook_ib_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"country": {"type": "text","similarity": "esserverbook_ib_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"date_added": {"type": "text","similarity": "esserverbook_ib_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"description": {"type": "text","similarity": "esserverbook_ib_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 1000}}},"director": {"type": "text","similarity": "esserverbook_ib_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"duration": {"type": "text","similarity": "esserverbook_ib_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"rating": {"type": "text", "similarity": "esserverbook_ib_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"release_year": {"type": "text","similarity": "esserverbook_ib_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}},"title": {"type": "text","similarity": "esserverbook_ib_similarity","fields" :{"keyword": {"type": "keyword","ignore_above": 256}}},"type": {"type": "text","similarity":"esserverbook_ib_similarity","fields": {"keyword": {"type": "keyword","ignore_above": 256}}}}}'
+
+ibmaptext = requests.put('http://localhost:9200/ibnetflix/_mapping?pretty' , headers=headers, data = ibmapping)
+
+print(ibtext.json())
+print(ibmaptext.json())
 
 
 
@@ -132,60 +190,30 @@ es.indices.refresh(index="ibnetflix")
 res_dfr = es.search(index="dfrnetflix", body={"query": {"match_all": {}}})
 print("Got %d Hits:" % res_dfr['hits']['total']['value'])
 
-print(DFRtext.json())
-DFRsettings = requests.get('http://localhost:9200/dfrnetflix/_settings?pretty' , headers=headers)
-print(DFRsettings.json())
-
-
 
 res_bm25 = es.search(index="bm25netflix", body={"query": {"match_all": {}}})
 print("Got %d Hits:" % res_bm25['hits']['total']['value'])
-
-print(bm25text.json())
-bm25settings = requests.get('http://localhost:9200/bm25netflix/_settings?pretty' , headers=headers)
-print(bm25settings.json())
 
 
 res_tfidf = es.search(index="tfidfnetflix", body={"query": {"match_all": {}}})
 print("Got %d Hits:" % res_tfidf['hits']['total']['value'])
 
-print(tfidftext.json())
-tfidfsettings = requests.get('http://localhost:9200/tfidfnetflix/_settings?pretty' , headers=headers)
-print(tfidfsettings.json())
-
 
 res_dfi = es.search(index="dfinetflix", body={"query": {"match_all": {}}})
 print("Got %d Hits:" % res_dfi['hits']['total']['value'])
 
-print(dfitext.json())
-dfisettings = requests.get('http://localhost:9200/tfidfnetflix/_settings?pretty' , headers=headers)
-print(dfisettings.json())
-
-
 
 res_lmd = es.search(index="lmdnetflix", body={"query": {"match_all": {}}})
 print("Got %d Hits:" % res_lmd['hits']['total']['value'])
-
-print(lmdtext.json())
-lmdsettings = requests.get('http://localhost:9200/lmdnetflix/_settings?pretty' , headers=headers)
-print(lmdsettings.json())
 
 
 res_lmj = es.search(index="lmjnetflix", body={"query": {"match_all": {}}})
 print("Got %d Hits:" % res_lmj['hits']['total']['value'])
 
 
-print(lmjtext.json())
-lmjsettings = requests.get('http://localhost:9200/lmjnetflix/_settings?pretty' , headers=headers)
-print(lmjsettings.json())
-
-
 res_ib = es.search(index="ibnetflix", body={"query": {"match_all": {}}})
 print("Got %d Hits:" % res_ib['hits']['total']['value'])
 
-print(ibtext.json())
-ibsettings = requests.get('http://localhost:9200/ibnetflix/_settings?pretty' , headers=headers)
-print(ibsettings.json())
 
 
 
