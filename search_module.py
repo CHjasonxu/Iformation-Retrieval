ebebed097b3ad307eba35c8f5c2b60455640a0af
CHjasonxu <38419317+CHjasonxu@users.noreply.github.com>
2020-04-10 15:03:17 +0100

    Add files via upload

--- search_module.py
+++ search_module.py
@@ -14,6 +14,14 @@ lmj_score = []
 tfidf_score =[]
 lmd_score =[]
 
+bm25_id = []
+dfi_id = []
+ib_id = []
+dfr_id = []
+lmj_id = []
+tfidf_id =[]
+lmd_id =[]
+
 ##bm25netflix
 s = Search(using=es, index="bm25netflix")
 query = 'The spider man collection of parallel universes'
@@ -22,7 +30,9 @@ results = s.query("simple_query_string", query=query, fields=fields, auto_genera
 # scan() removed
 for hit in results:
     bm25_score.append(hit.meta.score)
+    bm25_id.append(hit.meta.id)
 print('This is bm25_score:',bm25_score)
+print('This is bm25_id:',bm25_id)
 
 ##dfinetflix
 s = Search(using=es, index="dfinetflix")
@@ -32,7 +42,9 @@ results = s.query("simple_query_string", query=query, fields=fields, auto_genera
 # scan() removed
 for hit in results:
     dfi_score.append(hit.meta.score)
+    dfi_id.append(hit.meta.id)
 print('This is dfi_score:',dfi_score)
+print('This is dfi_id:',dfi_id)
 
 ##ibnetflix
 s = Search(using=es, index="ibnetflix")
@@ -42,7 +54,9 @@ results = s.query("simple_query_string", query=query, fields=fields, auto_genera
 # scan() removed
 for hit in results:
     ib_score.append(hit.meta.score)
+    ib_id.append(hit.meta.id)
 print('This is ib_score:',ib_score)
+print('This is ib_id:',ib_id)
 
 ##dfrnetflix
 s = Search(using=es, index="dfrnetflix")
@@ -52,7 +66,9 @@ results = s.query("simple_query_string", query=query, fields=fields, auto_genera
 # scan() removed
 for hit in results:
     dfr_score.append(hit.meta.score)
+    dfr_id.append(hit.meta.id)
 print('This is dfr_score:',dfr_score)
+print('This is dfr_id:',dfr_id)
 
 ##lmjnetflix
 s = Search(using=es, index="lmjnetflix")
@@ -62,7 +78,9 @@ results = s.query("simple_query_string", query=query, fields=fields, auto_genera
 # scan() removed
 for hit in results:
     lmj_score.append(hit.meta.score)
+    lmj_id.append(hit.meta.id)
 print('This is lmj_score:',lmj_score)
+print('This is lmj_id:',lmj_id)
 
 ##tfidfnetflix
 s = Search(using=es, index="tfidfnetflix")
@@ -72,7 +90,9 @@ results = s.query("simple_query_string", query=query, fields=fields, auto_genera
 # scan() removed
 for hit in results:
     tfidf_score.append(hit.meta.score)
+    tfidf_id.append(hit.meta.id)
 print('This is tfidf_score:',tfidf_score)
+print('This is tfidf_id:',tfidf_id)
 
 ##lmdnetflix
 s = Search(using=es, index="lmdnetflix")
@@ -82,4 +102,6 @@ results = s.query("simple_query_string", query=query, fields=fields, auto_genera
 # scan() removed
 for hit in results:
     lmd_score.append(hit.meta.score)
+    lmd_id.append(hit.meta.id)
 print('This is lmd_score:',lmd_score)
+print('This is lmd_id:',lmd_id)
