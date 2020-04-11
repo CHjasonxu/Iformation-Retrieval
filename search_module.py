08b4ae70f808038da0e38548e2074e66e46d6f56
CHjasonxu <38419317+CHjasonxu@users.noreply.github.com>
2020-04-11 13:57:37 +0100

    Add files via upload

--- search_module.py
+++ search_module.py
@@ -1,10 +1,12 @@
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search
+import numpy as np
 
 es = Elasticsearch(['localhost'],port=9200)
 
 label_fields = ["title"]
 fields = ["title","cast","country","description"]
+weight_fields =["title^2","cast^1","country^3","description^4"]
 
 label_bm25_id = []
 label_dfi_id = []
@@ -59,6 +61,19 @@ def Similarity_module(index,query,fields):
     # print('This is Similarity_id:', Similarity_id)
     return Similarity_score,Similarity_id
 
+def Similarity_module_weight(index,query,weight_fields):
+    Similarity_score=[]
+    Similarity_id =[]
+    s = Search(using=es, index=index)
+    results = s.query("simple_query_string", query=query, fields=fields,
+                      auto_generate_synonyms_phrase_query=True).execute()
+    for hit in results:
+        Similarity_score.append(hit.meta.score)
+        Similarity_id.append(hit.meta.id)
+    # print('This is Similarity_score:', Similarity_score)
+    # print('This is Similarity_id:', Similarity_id)
+    return Similarity_score,Similarity_id
+
 def cal_rec_pre(label_id,search_id):
     tmp = [val for val in label_id if val in search_id]
     precision = len(tmp) / len(search_id)
@@ -78,11 +93,16 @@ if __name__ == '__main__':
             precision, recall = cal_rec_pre(label_id,Similarity_id)
             tmp_precision = tmp_precision+precision
             tmp_recall = tmp_recall+recall
+        output_score = np.array(Similarity_score)
+        np.savetxt("%s_score"%index[i],output_score)
         Ag_precision.append(tmp_precision/len(label_query))
         Ag_recall.append(tmp_recall/len(label_query))
         tmp_precision=0
         tmp_recall=0
-    
+    ag_precision=np.array(Ag_precision)
+    ag_recall=np.array(Ag_recall)
+    np.savetxt("Average_precision",ag_precision)
+    np.savetxt("Average_recall",ag_recall)
     print("This is Ag_precision:",Ag_precision)
     print("This is Ag_recall:",Ag_recall)
 
