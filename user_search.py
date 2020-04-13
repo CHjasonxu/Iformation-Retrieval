05d0e33b524323fd6ff9cf47bf8edab20ae72073
Maz101 <43634935+Maz101@users.noreply.github.com>
2020-04-13 10:50:45 +0100

    Add files via upload

--- user_search.py
+++ user_search.py
@@ -46,7 +46,7 @@ def Standard_pattern():
     query = input(
         "Please input the query you want: ")  # Example: Indiana Jones tries to find Ark of Covenant
     indexs = ['lmjnetflix','tfidfnetflix', 'lmdnetflix']
-    weight_fields = ["title^2", "cast^1", "country^3", "description^4"]
+    weight_fields = ["title^2", "cast^1", "country^1", "description^4"]
     Get_standard_results(query, indexs, weight_fields)
 
 
@@ -57,7 +57,7 @@ def Advanced_pattern():
     print('0-BM25', '1-DFI', '2-IB', '3-DFR', '4-LMJ', '5-TF-IDF', '6-LMJ')
     while True:
         try:
-            indexs_selected = int(input("Please enter the number to select the model you want: "))
+            indexs_selected = int(input("Please enter the number to select the model, otherwise press Enter: "))
             indexs += [index[indexs_selected]]
         except:
             break
@@ -67,13 +67,13 @@ def Advanced_pattern():
           "7-duration", "8-description")
     while True:
         try:
-            fields_selected = int(input("Please select one field you want: "))
+            fields_selected = int(input("Please select one field you want if finished press Enter: "))
             field += [original_fields[fields_selected]]
         except:
             break
     print(field)
     print("----------------------")
-    print("Select the weight of each field")
+    print("Select the weight of each field, default is 1")
     for j in range(len(field)):
         weight_select = input("The value of weight: ")
         field[j] += '^' + weight_select
