ba250535c3521f69d26b71c5fc73ccb4e32cf861
Maz101 <43634935+Maz101@users.noreply.github.com>
2020-04-13 18:30:59 +0100

    Delete elasticshell2.sh

--- elasticshell2.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/CHjasonxu/Iformation-Retrieval.git
-
-curl -H "Content-Type: application/x-ndjson" -XPOST "localhost:9200/bank/_bulk?pretty&refresh" --data-binary "@NetflixDataJson.json"
-curl "localhost:9200/_cat/indices?v"
